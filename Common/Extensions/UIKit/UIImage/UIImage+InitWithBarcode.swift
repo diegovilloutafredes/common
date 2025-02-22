d7e6175f8112eed140dd6bad4984bba363a04dd9
Diego Villouta Fredes <dvillouta@jumpitt.com>
2025-02-22 04:01:33 -0300

    Version 1.0.16

--- Common/Extensions/UIKit/UIImage/UIImage+InitWithBarcode.swift
+++ Common/Extensions/UIKit/UIImage/UIImage+InitWithBarcode.swift
@@ -6,18 +6,13 @@
 import UIKit
 
 extension UIImage {
-    convenience init?(barcode: String) {
+    public convenience init?(barcode: String) {
         guard let filter = CIFilter(name: "CICode128BarcodeGenerator") else { return nil }
-
         let data = barcode.data(using: .ascii)
-
         filter.setValue(data, forKey: "inputMessage")
         filter.setValue(0, forKey: "inputQuietSpace")
-
         let transform = CGAffineTransform(scaleX: 3, y: 3)
-
         guard let ciImage = filter.outputImage?.transformed(by: transform) else { return nil }
-
         self.init(ciImage: ciImage)
     }
 }
--- Common/Extensions/UIKit/UIImageView/UIImageView+GenerateQRCode.swift
+++ Common/Extensions/UIKit/UIImageView/UIImageView+GenerateQRCode.swift
@@ -8,18 +8,13 @@ import UIKit
 extension UIImageView {
     public func generateQRCode(using string: String) {
         guard let filter = CIFilter(name: "CIQRCodeGenerator") else { return }
-
         let data = string.data(using: .ascii)
-
         filter.setValue(data, forKey: "inputMessage")
-
         guard let outputImage = filter.outputImage else { return }
-
         let scaleX = frame.size.width / outputImage.extent.size.width
         let scaleY = frame.size.height / outputImage.extent.size.height
         let transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
         let transformedOutputImage = outputImage.transformed(by: transform)
-
         image = .init(ciImage: transformedOutputImage)
     }
 }
